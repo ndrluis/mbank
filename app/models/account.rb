c102558426013bc88ce4f77c36a47733897600bd
Andre Anastacio <andreluisanastacio@gmail.com>
2019-08-04 19:35:23 -0300

    Add balance to account

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,2 +1,36 @@
 class Account < ApplicationRecord
+  has_many :debit_transfers,
+    -> { Transaction.transfer },
+    foreign_key: "source_account_id",
+    class_name: "Transaction"
+
+  has_many :credit_transfers,
+    -> { Transaction.transfer },
+    foreign_key: "destination_account_id",
+    class_name: "Transaction"
+
+  has_many :deposits,
+    -> { Transaction.deposit },
+    foreign_key: "source_account_id",
+    class_name: "Transaction"
+
+  def balance
+    deposits_balance +
+      credit_transfers_balance -
+      debit_transfers_balance
+  end
+
+  private
+
+  def credit_transfers_balance
+    credit_transfers.sum(:amount)
+  end
+
+  def debit_transfers_balance
+    debit_transfers.sum(:amount)
+  end
+
+  def deposits_balance
+    deposits.sum(:amount)
+  end
 end
--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -1,4 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Account, type: :model do
+  describe "#balance" do
+    it "returns the sum of the account transactions" do
+      source_account = Account.create
+      destination_account = Account.create
+
+      Transaction.create(
+        destination_account: source_account,
+        source_account: source_account,
+        amount: 1000.00,
+        kind: :deposit
+      )
+
+      Transaction.create(
+        source_account: source_account,
+        destination_account: destination_account,
+        amount: 100.00,
+        kind: :transfer
+      )
+
+      expect(destination_account.balance).to eq(100.00)
+      expect(source_account.balance).to eq(900.00)
+    end
+  end
 end
