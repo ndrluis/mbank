dc1713008a7600adf537139247c725e4dcd863c5
Andre Anastacio <andreluisanastacio@gmail.com>
2019-08-04 19:05:24 -0300

    Add deposits controller

---
+++ app/controllers/deposits_controller.rb
@@ -0,0 +1,26 @@
+class DepositsController < ApplicationController
+  def create
+    transaction = Transaction.new(transaction_params)
+
+    if transaction.save
+      render json: transaction, status: :created
+    else
+      render json: transaction.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def transaction_params
+    deposit_params.merge(
+      source_account_id: deposit_params[:destination_account_id],
+      kind: :deposit
+    )
+  end
+
+  def deposit_params
+    params
+      .require(:deposit)
+      .permit(:destination_account_id, :amount)
+  end
+end
---
+++ spec/request/deposits_spec.rb
@@ -0,0 +1,56 @@
+require "rails_helper"
+
+RSpec.describe "Deposits", type: :request do
+  describe "POST /deposits" do
+    context "with valid params" do
+      let(:account) { Account.create }
+
+      it "returns 201 created" do
+        post "/deposits", params: {
+          deposit: { destination_account_id: account.id, amount: 1000.50 }
+        }
+
+        expect(response).to have_http_status(:created)
+      end
+
+      it "returns transaction information" do
+        post "/deposits", params: {
+          deposit: { destination_account_id: account.id, amount: 1000.55 }
+        }
+
+        expect(response_json).to include(
+          {
+            "amount" => "1000.55",
+            "kind" => "deposit",
+            "source_account_id" => account.id,
+            "destination_account_id" => account.id
+          }
+        )
+      end
+    end
+
+    context "with invalid params" do
+      let(:account) { Account.create }
+
+      it "returns 422 unprocessable entity" do
+        post "/deposits", params: {
+          deposit: { destination_account_id: account.id }
+        }
+
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
+
+      it "returns transaction information" do
+        post "/deposits", params: {
+          deposit: { destination_account_id: account.id }
+        }
+
+        expect(response_json).to match(
+          {
+            "amount" => ["can't be blank"]
+          }
+        )
+      end
+    end
+  end
+end
