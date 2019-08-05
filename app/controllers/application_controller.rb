637c28cf85ac2497f22d622fedf633fface0901e
Andre Anastacio <andreluisanastacio@gmail.com>
2019-08-04 23:42:48 -0300

    Add rescue not found to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::API
+  rescue_from 'ActiveRecord::RecordNotFound' do |exception|
+    render json: { error: exception.message }, status: 404
+  end
 end
