b9d8280cfb7bce18e9820f1c083be76b17842418
Andre Anastacio <andreluisanastacio@gmail.com>
2019-08-04 22:44:03 -0300

    Add transfer destination validation to transaction

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -4,10 +4,17 @@ class Transaction < ApplicationRecord
 
   validates :amount, :kind, presence: true
   validate :enough_balance_to_transfer, if: :transfer?
+  validate :transfer_to_destination, if: :transfer?
 
   enum kind: %w(transfer deposit)
 
   private
+
+  def transfer_to_destination
+    return if source_account_id != destination_account_id
+
+    errors.add(:destination_account, "can't be the same of source account")
+  end
 
   def enough_balance_to_transfer
     return if source_account.enough_balance_to_transfer?(amount)
--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -4,6 +4,78 @@ RSpec.describe Transaction, type: :model do
   it { is_expected.to define_enum_for(:kind).with_values(%w(transfer deposit)) }
 
   describe "validations" do
+    describe "#transfer_to_destination" do
+      context "when transaction kind is transfer" do
+        context "when source and destination account is the same" do
+          it "adds error message" do
+            account = Account.create
+
+            Transaction.create(
+              source_account: account,
+              destination_account: account,
+              amount: 110.50,
+              kind: :deposit
+            )
+
+            transaction = Transaction.new(
+              source_account: account,
+              destination_account: account,
+              amount: 110.50,
+              kind: :transfer
+            )
+
+            transaction.valid?
+
+            expect(transaction.errors.messages).to eq(
+              { destination_account: ["can't be the same of source account"] }
+            )
+          end
+        end
+
+        context "when source and destination account is different" do
+          it "does not add error message" do
+            source_account = Account.create
+            destination_account = Account.create
+
+            Transaction.create(
+              source_account: source_account,
+              destination_account: source_account,
+              amount: 110.50,
+              kind: :deposit
+            )
+
+            transaction = Transaction.new(
+              source_account: source_account,
+              destination_account: destination_account,
+              amount: 110.50,
+              kind: :transfer
+            )
+
+            transaction.valid?
+
+            expect(transaction.errors).to be_empty
+          end
+        end
+      end
+
+      context "when transaction kind is deposit" do
+        it "skips validation" do
+          source_account = Account.create
+
+          transaction = Transaction.new(
+            source_account: source_account,
+            destination_account: source_account,
+            amount: 110.50,
+            kind: :deposit
+          )
+
+          transaction.valid?
+
+          expect(transaction.errors).to be_empty
+        end
+      end
+    end
+
     describe "#enough_balance_to_transfer" do
       context "when transation kind is transfer" do
         context "with transfer amount greater than balance" do
@@ -11,7 +83,7 @@ RSpec.describe Transaction, type: :model do
             source_account = Account.create
             destination_account = Account.create
 
-            transaction = Transaction.create(
+            Transaction.create(
               source_account: source_account,
               destination_account: source_account,
               amount: 110.50,
